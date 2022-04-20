09864cda15708e8f621b6841d71305036e408b36
natsumi <aka.723n@gmail.com>
2022-04-20 18:04:11 +0900

    add: エラー文の出力

--- src/App.jsx
+++ src/App.jsx
@@ -27,20 +27,25 @@ function App() {
   const validate = (values) => {
     //
     const errors = {};
-    //usernameの値が存在しなかったら
+    const regex =
+      /^[a-zA-Z0-9_.+-]+@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/;
+    //usernameの値が存在しなかったらerrorsの配列に入れる。
     if(!values.username){
       errors.username = 'ユーザー名を入力してください';
     }
     if(!values.mailAddress){
-      errors.username = 'ユーザー名を入力してください';
+      errors.mailAddress = 'メールアドレスを入力してください';
+    } else if(!regex.test(values.mailAddress)){
+      errors.mailAddress = "正しいメールアドレスを入力してくださいい";
     }
     if(!values.password){
-      errors.username = 'ユーザー名を入力してください';
-    }else if(values.password.length> 4 ){
-      errors.username = '4文字以上15文字以下のパスワードを入力してください';
+      errors.password = 'パスワードを入力してください';
+    } else if(values.password.length < 4 ){
+      errors.password = '4文字以上15文字以下のパスワードを入力してください';
     } else if(values.password.length> 15 ){
-      errors.username = '4文字以上15文字以下のパスワードを入力してください';
+      errors.password = '4文字以上15文字以下のパスワードを入力してください';
     }
+    return errors;
   }
 
   return (
@@ -53,16 +58,21 @@ function App() {
             <label>ユーザー名</label>
             <input type="text" placeholder="ユーザー名" name="username" onChange={(e)=>handleChange(e)}/>
           </div>
+          <p className="errorMsg">{formErrors.username}</p>
           <div className="formField">
             <label>メールアドレス</label>
             <input type="text" placeholder="メールアドレス" name="mailAddress" onChange={(e)=>handleChange(e)}/>
           </div>
+          <p className="errorMsg">{formErrors.mailAddress}</p>
           <div className="formField">
             <label>パスワード</label>
             <input type="text" placeholder="パスワード" name="password" onChange={(e)=>handleChange(e)}/>
           </div>
+          <p className="errorMsg">{formErrors.password}</p>
+          <button className="submitButton">ログイン</button>
         </div>
-        <button className="submitButton">ログイン</button>
+
+
       </form>
 
     </div>

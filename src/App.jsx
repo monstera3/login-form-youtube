53acd6a01109fb29924dbd8bf8336fea5e15409f
natsumi <aka.723n@gmail.com>
2022-04-20 17:14:43 +0900

    add: inputに入力するとどこに入ってきたか識別する

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const initialValues  = {username:"",mailAddress:"",password:""};
+  const [formValues,setFromValues] = useState(initialValues);
+
+  const handleChange = (e) => {
+    //e.target とは、ユーザー名のインプット属性にアクセスしているのかメールアドレスなのか識別するために要素を取得している
+    // targetの中にname属性やvalue属性が入っている
+    //分割代入↓
+    const { name,value } = e.target;
+    setFromValues({...formValues, [name]: value});
+    console.log(formValues);
+  }
+
+
   return (
     <div className="formContainer">
       <form>
@@ -10,15 +23,15 @@ function App() {
         <div className="uiForm">
           <div className="formField">
             <label>ユーザー名</label>
-            <input type="text" placeholder="ユーザー名" name="username"/>
+            <input type="text" placeholder="ユーザー名" name="username" onChange={(e)=>handleChange(e)}/>
           </div>
           <div className="formField">
             <label>メールアドレス</label>
-            <input type="text" placeholder="メールアドレス" name="mailAddress"/>
+            <input type="text" placeholder="メールアドレス" name="mailAddress" onChange={(e)=>handleChange(e)}/>
           </div>
           <div className="formField">
             <label>パスワード</label>
-            <input type="text" placeholder="パスワード" name="password"/>
+            <input type="text" placeholder="パスワード" name="password" onChange={(e)=>handleChange(e)}/>
           </div>
         </div>
         <button className="submitButton">ログイン</button>

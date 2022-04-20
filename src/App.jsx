7bb2128ecf531f787fc5a7c134cea2690f92aec1
natsumi <aka.723n@gmail.com>
2022-04-20 16:39:56 +0900

    add: 背景と基本の形作成

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,25 @@ import './App.css';
 function App() {
   return (
     <div className="formContainer">
-      from
+      <form>
+        <h1>ログインフォーム</h1>
+        <hr/>
+        <div className="uiForm">
+          <div className="formField">
+            <label>ユーザー名</label>
+            <input type="text" placeholder="ユーザー名" name="username"/>
+          </div>
+          <div className="formField">
+            <label>メールアドレス</label>
+            <input type="text" placeholder="メールアドレス" name="mailAddress"/>
+          </div>
+          <div className="formField">
+            <label>パスワード</label>
+            <input type="text" placeholder="パスワード" name="password"/>
+          </div>
+        </div>
+        <button className="submitButton">ログイン</button>
+      </form>
 
     </div>
   );
